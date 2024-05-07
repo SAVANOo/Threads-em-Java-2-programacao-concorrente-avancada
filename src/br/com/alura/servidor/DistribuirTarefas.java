ad994fe55614f612d9c97f340e590fbf3afa7291
SAVANOo <savano947@gmail.com>
2024-05-07 11:05:28 -0300

    Feat: devolvendo para o cliente uma confirmação

--- src/br/com/alura/servidor/DistribuirTarefas.java
+++ src/br/com/alura/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.alura.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -16,10 +17,14 @@ public class DistribuirTarefas implements Runnable {
         try {
             System.out.println("Distribuindo tarefas para " + socket);
             Scanner entradaCliente = new Scanner(socket.getInputStream());
+            PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+
             while (entradaCliente.hasNextLine()) {
                 String comando = entradaCliente.nextLine();
                 System.out.println(comando);
+                saidaCliente.println("Comando \"" + comando + "\" recebido");
             }
+            saidaCliente.close();
             entradaCliente.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
